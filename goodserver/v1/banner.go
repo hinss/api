0d9981ed92f60a50f1179282c22d25740c8556be
raymond <raymond.zhang@alphaliongroup.com>
2022-04-03 15:28:14 +0800

    add banners model

--- goodserver/v1/banner.go
+++ goodserver/v1/banner.go
@@ -1,5 +1,17 @@
 package v1
 
-type Banner struct {
+import "time"
 
+type Banners struct {
+	Id int `gorm:"column:id"`
+
+	Name string `gorm:"column:name"`
+
+	Logo string `gorm:"column:logo"`
+
+	AddTime time.Time `gorm:"column:add_time"`
+
+	IsDeleted uint8 `gorm:"column:is_deleted"`
+
+	UpdateTime time.Time `gorm:"column:update_time"`
 }
