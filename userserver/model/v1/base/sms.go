dba7ab1646048f0d7a5483562387de3904694bdf
raymond <raymond.zhang@alphaliongroup.com>
2022-05-28 17:38:01 +0800

    sms form

---
+++ userserver/model/v1/base/sms.go
@@ -0,0 +1,7 @@
+package forms
+
+type SendSmsForm struct {
+	Mobile string `form:"mobile" json:"mobile" binding:"required,mobile"`
+	Type   int    `form:"type" json:"type" binding:"required,oneof=1 2"`
+	// type 1-注册 2登录
+}
